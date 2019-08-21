072cde2dbf45872ff7891fd6f19f0c08f55cc0f1
tobyjamez <tobyswjames@gmail.com>
2019-08-21 09:33:26 +0200

    Update getblablacars.py to allow to specify rides to a certain city

--- python/blablacar/getblablacars.py
+++ python/blablacar/getblablacars.py
@@ -13,11 +13,14 @@ HELP_TEXT = "\nType your origin city.\n"\
             "If any of the options interest you, enter their letter\n"\
             "to view the webpage.\n"\
             "Else, enter q to quit.\n"\
+            "To find blablacars to a city, prefix the city name with"\
+            " \"#t\", eg #tBerlin.\n"\
             "\nThe program sometimes interprets weird strings as"\
             "cities.\nThis is on blablacar's end.\n"
 
 
-def parse_results(response: requests.models.Response) -> dict:
+def parse_results(response: requests.models.Response,
+                  to=False) -> dict:
     """
     Parse a response from a requests.get call into a suitable dictionary
     for links.
@@ -25,21 +28,26 @@ def parse_results(response: requests.models.Response) -> dict:
 
     :param response: The response from a requests.get call to blablacar.
     :type response: requests.models.Response.
+    :param to: If True, prints departure cities for each car, rather
+               than arrivals.
+    :type to: bool.
 
     :returns: A dictionary of letters and urls to blablacar.
     :rtype: dict.
     """
 
+    places = ['arrival_place', 'departure_place']
+
     url_dict = {}
 
     results = response.json()
     print("Interpreted your input as: ",
-          results['trips'][0]['departure_place']['city_name'].strip(), '.')
+          results['trips'][0][places[int(~to)]]['city_name'].strip(), '.')
     print("The following trips are available:\n")
 
     for index, trip in enumerate(results['trips']):
         print("  [%s]" % ALPHABET[index],
-              trip['arrival_place']['city_name'], ":")
+              trip[places[int(to)]]['city_name'], ":")
 
         print("    Costs:",
               "%.2f" % float(trip['price_with_commission']['value'] + 1),
@@ -58,13 +66,32 @@ if __name__ == "__main__":
 
     origin = input("Please enter a city or type help for help:\n$ ").title()
 
+    if origin.lower() == 'q':
+        exit()
+
     if origin.lower() == 'help':
         print(HELP_TEXT)
         origin = input("Please enter a city:\n$ ").title()
 
-    params = dict(fn=origin,
-                  de=datetime.datetime.today().strftime('%Y-%m-%d'),
+    params = dict(de=datetime.datetime.today().strftime('%Y-%m-%d'),
                   limit=25)
+
+    if origin[0] == '#':
+        if origin[1].lower() == 't':
+            origin = origin[2:].title()
+            params['tn'] = origin
+            place = 'tn'
+            to = True
+
+        else:
+            place = 'fn'
+            to = False
+
+    else:
+        params['fn'] = origin
+        place = 'fn'
+        print(place)
+        to = False
 
     while True:
         response = requests.get(API_URL,
@@ -72,13 +99,13 @@ if __name__ == "__main__":
                                 headers=dict(Key=key))
 
         if response.status_code != 200:
-            params['fn'] = input("Sorry, I couldn't find that city.\n"
-                                 "Please enter another.\n$ ")
+            params[place] = input("Sorry, I couldn't find city. %r \n"
+                                  "Please enter another.\n$ " % origin).title()
         else:
             break
 
     try:
-        url_dict = parse_results(response)
+        url_dict = parse_results(response, to=to)
     except(IndexError):
         print("Sorry. No trips found.")
         exit()
